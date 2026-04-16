283d0660c78d350c028f987e7a76fd8dd5e61f2e
César <usr3965@salesianos-lacuesta.net>
2026-04-16 13:31:38 +0100

    style(colors): adding the colors

---
+++ theme/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  primaryRed: "#e74c3c",
+  primaryGreen: "#2da42f",
+  primaryBlue: "#2980b9",
+  textWhite: "#fff",
+  backgroundWhite: "#fde8e8",
+  primaryText: "#333",
+};
